39757e43ab9cd339cb1f8effcd4f9fa8cc98954b
Cal Courtney <cal@absmartly.com>
2022-09-13 12:37:47 +0100

    Gave better attributes examples

--- docs/sdk-documentation/getting-started/setting-context-attributes/js/setAttributes.js
+++ docs/sdk-documentation/getting-started/setting-context-attributes/js/setAttributes.js
@@ -1,13 +1,17 @@
 context.attribute("user_agent", navigator.userAgent);
 
 context.attributes({
-  customer_age: "new_customer",
+  customer_time:
+    user.created > new Date().getTime() - 24 * 60 * 60 * 1000
+      ? "new_customer"
+      : "returning_customer",
+  user_type: user.isInternal ? "internal" : "normal",
   url: location.toString(),
+  user_agent: navigator.userAgent,
   referrer: document.referrer,
-  screenName: "...",
-  pageName: "...",
-  blockName: "...",
-  country: "gb",
+  screenName,
+  pageName,
+  country: headers["HTTP_CF_IPCOUNTRY"],
   language: headers["Accept-Language"],
-  channel: "google-ads",
+  channel: query_params.utm_medium,
 });
