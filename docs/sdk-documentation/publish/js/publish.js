0a6715f4f0b12773159941f2fba90d1be8efd193
Cal Courtney <cal@absmartly.com>
2022-09-13 09:56:58 +0100

    Rewrote publish & tracking goals

--- docs/sdk-documentation/publish/js/publish.js
+++ docs/sdk-documentation/publish/js/publish.js
@@ -1,8 +1,9 @@
 // You can just publish
 context.publish();
+
 // or wait for it to finish, so if you want to
 // navigate to another page without losing impressions,
-// you  can do that once the promise resolves.
+// you can do that once the promise resolves.
 context.publish().then(function () {
   document.location.replace("another_page");
 });
--- docs/sdk-documentation/tracking-goals/js/trackingGoals.js
+++ docs/sdk-documentation/tracking-goals/js/trackingGoals.js
@@ -1,9 +1,8 @@
-context.track("goal_name", { ...properties });
-
 var properties = {
   price: 10000,
   category: "5 stars",
   free_cancellation: true,
   instance_id: 5350,
 };
+
 context.track("booking", properties);
