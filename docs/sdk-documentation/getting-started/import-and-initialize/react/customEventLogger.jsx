e9d2e511dd510d5d3956f5d84a3b9e24780247c9
Cal Courtney <cal@absmartly.com>
2022-09-22 14:14:33 +0100

    Added react custom event logger docs

---
+++ docs/sdk-documentation/getting-started/import-and-initialize/react/customEventLogger.jsx
@@ -0,0 +1,18 @@
+ReactDOM.render(
+  <ABSmartly
+    sdkOptions={{
+      endpoint: "https://sandbox.absmartly.io/v1",
+      apiKey: process.env.ABSMARTLY_API_KEY,
+      application: process.env.APPLICATION_NAME,
+      environment: process.env.NODE_ENV,
+      eventLogger: (context, eventName, data) => {
+        if (eventName == "error") {
+          console.error(data);
+        }
+      },
+    }}
+  >
+    <App />
+  </ABSmartly>,
+  document.getElementById("root")
+);
