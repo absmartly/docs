652891504618383ac8f3a399eca552cf347f4d58
Cal Courtney <cal@absmartly.com>
2022-09-22 14:15:41 +0100

    Added defaultButtonColor variable

--- docs/sdk-documentation/basic-usage/selecting-a-variable/react/directlyInTreatmentComponent.js
+++ docs/sdk-documentation/basic-usage/selecting-a-variable/react/directlyInTreatmentComponent.js
@@ -1,10 +1,14 @@
+const defaultButtonColorValue = "red";
+
 return (
   <Treatment context={context} name="experiment_name">
     {({ variant, variables, loading }) =>
       loading ? (
         <MySpinner />
       ) : (
-        <ButtonComponent buttonColor={variables["button.color"] || "red"} />
+        <ButtonComponent
+          buttonColor={variables["button.color"] || defaultButtonColorValue}
+        />
       )
     }
   </Treatment>
