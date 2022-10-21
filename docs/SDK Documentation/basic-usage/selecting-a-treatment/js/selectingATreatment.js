bf12eb6f855197052aeed1a1b9f26c651b9f03e3
Cal Courtney <cal@absmartly.com>
2022-10-21 14:33:47 +0100

    Reformatted js indentation

--- docs/SDK Documentation/basic-usage/selecting-a-treatment/js/selectingATreatment.js
+++ docs/SDK Documentation/basic-usage/selecting-a-treatment/js/selectingATreatment.js
@@ -1,23 +1,22 @@
-context.ready().then(function() {
-
-// A/B/C Test
- if (context.treatment("experiment_name") == 1) {
-     // insert code to show on variant 1
- } else if (context.treatment("experiment_name") == 2) {
-     // insert code to show on variant 2
- } else {
-     // insert the control treatment code
- }
+context.ready().then(function () {
+  // A/B/C Test
+  if (context.treatment("experiment_name") == 1) {
+    // insert code to show on variant 1
+  } else if (context.treatment("experiment_name") == 2) {
+    // insert code to show on variant 2
+  } else {
+    // insert the control treatment code
+  }
 });
 
 // or using async/await
-async function() {
-    await context.ready();
+async function runExperiment() {
+  await context.ready();
 
-    // A/B Test
-    if (context.treatment("experiment_name")) {
-        // insert code to show on variant 1
-    } else {
-        // insert the control treatment code
-    }
+  // A/B Test
+  if (context.treatment("experiment_name")) {
+    // insert code to show on variant 1
+  } else {
+    // insert the control treatment code
+  }
 }
