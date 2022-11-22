a06710cc9eb93beac44786c473b08731404fb55f
Cal Courtney <cal@absmartly.com>
2022-11-22 15:59:43 +0000

    Added new features from react-sdk@2.0

--- docs/SDK Documentation/getting-started/create-new-context-request/react/newContext.js
+++ docs/SDK Documentation/getting-started/create-new-context-request/react/newContext.js
@@ -3,20 +3,7 @@ import { useABSmartly } from "react-sdk";
 
 function App() {
   // Get the SDK object using the useABSmartly hook
-  const sdk = useABSmartly();
-
-  // Define a new context request
-  const request = {
-    units: {
-      userId: "123",
-      session_id: "5ebf06d8cb5d8137290c4abb64155584fbdb64d8",
-      email: "support@absmartly.com", // strings will be hashed
-      deviceId: "345",
-    },
-  };
-
-  // Create a context
-  const context = sdk?.createContext(request);
+  const { context } = useABSmartly();
 
   return (
     <div className="App">
--- docs/SDK Documentation/getting-started/import-and-initialize/react/import.jsx
+++ docs/SDK Documentation/getting-started/import-and-initialize/react/import.jsx
@@ -4,6 +4,15 @@ import App from "./App";
 
 import ABSmartly from "@absmartly/react-sdk";
 
+const contextOptions = {
+  units: {
+    userId: "123",
+    session_id: "5ebf06d8cb5d8137290c4abb64155584fbdb64d8",
+    email: "support@absmartly.com",
+    deviceId: "345",
+  },
+};
+
 ReactDOM.render(
   <ABSmartly
     sdkOptions={{
@@ -12,6 +21,7 @@ ReactDOM.render(
       application: process.env.APPLICATION_NAME,
       environment: process.env.NODE_ENV,
     }}
+    contextOptions={contextOptions}
   >
     <App />
   </ABSmartly>,
