4f99b58750b0264ae7881cd33826d31cfd59f5dd
Cal Courtney <cal@absmartly.com>
2022-12-05 15:50:22 +0000

    Added PHP Advanced Docs

---
+++ docs/SDK Documentation/Advanced/context-attributes/setting-context-attributes/php/setAttributes.php
@@ -0,0 +1,6 @@
+$context->setAttribute('session_id', \session_id());
+$context->setAttributes(
+    [
+        'customer_age' => 'new_customer'
+    ]
+);
---
+++ docs/SDK Documentation/Advanced/custom-assignments/php/customAssignments.php
@@ -0,0 +1,7 @@
+$assignments = [
+    "experiment_name" => 1,
+    "another_experiment_name" => 0,
+    "a_third_experiment_name" => 2
+];
+
+$context->setCustomAssignments($assignments);
