4b9dd18a07896a1338e1fdda21c34c613094430d
Cal Courtney <cal@absmartly.com>
2023-08-14 15:20:51 +0100

    Updated colors and logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,24 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import ThemedImage from "@theme/ThemedImage"
 
 import styles from "./index.module.scss";
 
 const HomepageHeader: FC<{}> = () => {
   const { siteConfig } = useDocusaurusContext();
+
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <img
-          src="img/white_logo.png"
-          alt="The A B Smartly Logo"
-          className={styles.heroLogo}
-        />
+        <ThemedImage
+            alt="The A B Smartly Logo"
+            sources={{
+                light: "img/white_logo.png",
+                dark: "img/logo.png",
+            }}
+            className={styles.heroLogo}
+            />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className="styles"></div>
