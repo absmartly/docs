7513b59a155c18792a26f9b85db92b34acf48e0d
Jonas Alves <jonas@absmartly.com>
2026-02-26 21:39:22 +0000

    docs: update Go SDK snippets with idiomatic field names and New() constructor
    Replace trailing-underscore fields (Endpoint_, ApiKey_, Units_) with
    idiomatic Go names (Endpoint, APIKey, Units). Fix RefreshInteval_ typo.
    Add sdk.New() one-liner alternative.

--- docs/APIs-and-SDKs/SDK-Documentation/getting-started/create-new-context-request/go/refreshWithFreshData.go
+++ docs/APIs-and-SDKs/SDK-Documentation/getting-started/create-new-context-request/go/refreshWithFreshData.go
@@ -1,4 +1,4 @@
-var contextConfig = ContextConfig{Units_: map[string]string{
+var contextConfig = ContextConfig{Units: map[string]string{
 		"session_id": "bf06d8cb5d8137290c4abb64155584fbdb64d8",
 		"user_id":    "123456",
-}, RefreshInteval_: 4 * Time.Hour}
+}, RefreshInterval: 4 * Time.Hour}
