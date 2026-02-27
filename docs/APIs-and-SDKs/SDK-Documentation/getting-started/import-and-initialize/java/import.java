b5b29a13440d321fa8efbec5543f36289ae45f6e
Jonas Alves <jonas@absmartly.com>
2026-02-27 08:48:18 +0000

    fix: update Java snippet to use Builder pattern

--- docs/APIs-and-SDKs/SDK-Documentation/getting-started/import-and-initialize/java/import.java
+++ docs/APIs-and-SDKs/SDK-Documentation/getting-started/import-and-initialize/java/import.java
@@ -3,12 +3,12 @@ import com.absmartly.sdk.*;
 public class Example {
     static public void main(String[] args) {
 
-        final ABSmartly sdk = ABSmartly.create(
-            "https://your-company.absmartly.io/v1",
-            System.getenv("ABSMARTLY_APIKEY"),
-            "website",
-            "production"
-        );
+        final ABsmartly sdk = ABsmartly.builder()
+            .endpoint("https://your-company.absmartly.io/v1")
+            .apiKey(System.getenv("ABSMARTLY_APIKEY"))
+            .application("website")
+            .environment("production")
+            .build();
         // ...
     }
 }
