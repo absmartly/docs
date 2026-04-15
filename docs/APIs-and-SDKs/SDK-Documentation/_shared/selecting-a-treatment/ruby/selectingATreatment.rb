034369751504363e39deb3360120ab0bf93fba2c
Cal Courtney <calthejuggler@gmail.com>
2026-04-15 11:02:41 +0100

    fix(ft-1848): use an else block for control variant

--- docs/APIs-and-SDKs/SDK-Documentation/_shared/selecting-a-treatment/ruby/selectingATreatment.rb
+++ docs/APIs-and-SDKs/SDK-Documentation/_shared/selecting-a-treatment/ruby/selectingATreatment.rb
@@ -1,7 +1,7 @@
 treatment = context.treatment('homepage_banner_experiment')
 
-if treatment.zero?
-  show_banner("Welcome back!")
-else
+if treatment == 1
   show_banner("Check out our latest deals!")
+else
+  show_banner("Welcome back!")
 end
